5faecda77d236ccf620fe39cc327caa3dd5ffefc
thogue12 <hoguetimothy12@gmail.com>
2025-05-22 01:45:19 -0400

    another one

--- terraform/acr_aks.tf
+++ terraform/acr_aks.tf
@@ -22,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "this_aks_cluster" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_A2_v2"
   }
 
   identity {
