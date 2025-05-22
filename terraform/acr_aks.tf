5ba0fcec168381828203adff08cab5475103ccfb
thogue12 <hoguetimothy12@gmail.com>
2025-05-22 01:23:14 -0400

    commiting

--- terraform/acr_aks.tf
+++ terraform/acr_aks.tf
@@ -1,60 +1,65 @@
-### Creat a cert for the AKS cluster ###
-resource "tls_private_key" "agent" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-resource "tls_self_signed_cert" "this_agent" {
-  private_key_pem = tls_private_key.agent.private_key_pem
+# ### Creat a cert for the AKS cluster ###
+# resource "tls_private_key" "agent" {
+#   algorithm = "RSA"
+#   rsa_bits  = 2048
+# }
+# resource "tls_self_signed_cert" "this_agent" {
+#   private_key_pem = tls_private_key.agent.private_key_pem
   
 
-  subject {
-    common_name  = "devsecops-agent"
-    organization = "DevSecOps blueprint"
-  }
+#   subject {
+#     common_name  = "devsecops-agent"
+#     organization = "DevSecOps blueprint"
+#   }
 
-  validity_period_hours = 12
-  is_ca_certificate = false
+#   validity_period_hours = 12
+#   is_ca_certificate = false
 
-  allowed_uses = [
-    "key_encipherment",
-    "digital_signature",
-    "server_auth",
-    "client_auth"
-  ]
-}
+#   allowed_uses = [
+#     "key_encipherment",
+#     "digital_signature",
+#     "server_auth",
+#     "client_auth"
+#   ]
+# }
+
+
 resource "azurerm_resource_group" "this_resource_group" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_container_registry" "this_acr" {
+resource "azurerm_container_registry" "this_container_registry" {
   name                = var.acr_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  sku                 = "Premium"
-  admin_enabled       = false
-  georeplications {
-    location                = "West Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
+  sku                 = "Basic"
 }
-resource "azurerm_arc_kubernetes_cluster" "example" {
-  name                         = var.aks_name
-  resource_group_name          = var.resource_group_name
-  location                     = var.location
-  agent_public_key_certificate = tls_self_signed_cert.this_agent.cert_pem
+
+resource "azurerm_kubernetes_cluster" "this_aks_cluster" {
+  name                = var.aks_name
+  location            = var.resource_group_name
+  resource_group_name = var.location
+
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
 
   identity {
     type = "SystemAssigned"
   }
 
   tags = {
-    ENV = "Test"
+    Environment = "Production"
   }
+}
+
+resource "azurerm_role_assignment" "this_role_ass" {
+  principal_id                     = azurerm_kubernetes_cluster.this_aks_cluster.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.this_container_registry.id
+  skip_service_principal_aad_check = true
 }
