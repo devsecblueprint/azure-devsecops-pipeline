4e561024d654e339ac663d69f11aeee344c7028f
thogue12 <hoguetimothy12@gmail.com>
2025-05-22 00:26:05 -0400

    another one

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,18 @@
 
+variable "tfc_azure_dynamic_credentials" {
+  description = "Object containing Azure dynamic credentials configuration"
+  type = object({
+    default = object({
+      client_id_file_path = string
+      oidc_token_file_path = string
+    })
+    aliases = map(object({
+      client_id_file_path = string
+      oidc_token_file_path = string
+    }))
+  })
+}
+
 ### Resource Group Variables ###
 
 variable "resource_group_name" {
